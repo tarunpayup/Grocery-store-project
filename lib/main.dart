716b0eb2dcc0698d0bdddfc35b62f4a73924e80f
tarun <tarunpayup@outlook.com>
2026-07-12 12:44:13 +0530

    New push 12/07/2026 main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/view/home_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'viewmodels/user_view_model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserViewModel(),
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  Widget build(BuildContext context){
-    return HomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MVVM Provider Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
   }
 }
